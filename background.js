6fc7bfb8458cac99c52ae2d5dfda0438ee7d6231
Punxnot <do-you-speak@yandex.ru>
2021-02-16 00:41:12 +0300

    Remove unnecessary permissions

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// For the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
-	});
-});
-
 chrome.runtime.onMessage.addListener(
   function(url, sender, onSuccess) {
     fetch(url)
